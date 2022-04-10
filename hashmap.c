c6a597a3c82ebb7ae7560919cdb0f23e6f4d7adc
vicentesarabia <>
2022-04-10 22:20:54 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -41,7 +41,7 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map, char * key, void * value) {
     
-  size_t index=hash(key,map->capacity)
+  size_t index=hash(key,map->capacity);
 
   while(map->buckets[index].key != NULL)
     {
