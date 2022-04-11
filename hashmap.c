cca83d8c6e9c422f9d2ff0f568f362d067d06d4a
vicentesarabia <>
2022-04-11 01:11:02 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -45,7 +45,7 @@ void insertMap(HashMap * map, char * key, void * value) {
   int index=hash(key,map->capacity);
   if(map->buckets[index]==NULL)
   {
-    
+    if(map->capacity==map->size)return;
     map->buckets[index]=item;
     map->size++;
   }
